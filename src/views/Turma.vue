de18fff65787b21f6308415a6157b14887e8f68e
jadercerqueira <jader58@live.com>
2024-04-15 13:32:03 -0300

    ➕ - Separação de Turma

--- src/views/Turma.vue
+++ src/views/Turma.vue
@@ -25,13 +25,13 @@
               </div>
               <v-divider></v-divider>
 
-              <v-btn-toggle class="d-flex flex-column w-100 h-100" color="blue-lighten-4" rounded="0">
+              <v-btn-toggle class="d-flex flex-column w-100 h-100" color="blue-lighten-4" rounded="0" v-model="toggleTurma">
 
-                <v-btn class="pa-4">
+                <v-btn class="pa-4" value="t1" @click="selecionaTurma('LP1')" >
                   Turma 1
                 </v-btn>
 
-                <v-btn class="pa-4">
+                <v-btn class="pa-4" value="t2" @click="selecionaTurma('LP2')" >
                   Turma 2
                 </v-btn>
 
@@ -144,7 +144,7 @@
                       </v-col>
 
                       <v-col cols="3">
-                        <Chart :chartData="item.chartData" />
+                        <Chart :chartData="item.chartData"/>
                       </v-col>
                     </v-row>
 
@@ -185,33 +185,60 @@ export default {
     listaTurma: [],
     carregandoTurmas: true,
     chipValue: '',
-    toggle: undefined
+    toggle: undefined,
+    toggleTurma: undefined
 
 
   }),
 
   created() {
-    this.returnDadosTurma()
-      .then((dados) => {
-        // console.log(dados)
 
-        // Quando a promessa for resolvida, atribua os dados à listaTurma
-        this.listaTurma = dados.turma;
-        this.listaTurma = this.getChartDatas(this.listaTurma);
-      })
-      .catch((error) => {
-        // Em caso de erro, trate-o de acordo com a sua necessidade
-        console.error("Erro ao buscar dados da turma:", error);
-      }).finally(() => {
-        // Define carregandoTurmas como false quando a promessa for resolvida ou rejeitada
-        this.carregandoTurmas = false;
-      });
+    // this.returnDadosTurma().then((dados) => {
+    //     // console.log(dados)
+
+    //     // Quando a promessa for resolvida, atribua os dados à listaTurma
+    //     this.listaTurma = dados.turma;
+    //     this.listaTurma = this.getChartDatas(this.listaTurma);
+    //   })
+    //   .catch((error) => {
+    //     // Em caso de erro, trate-o de acordo com a sua necessidade
+    //     console.error("Erro ao buscar dados da turma:", error);
+    //   }).finally(() => {
+    //     // Define carregandoTurmas como false quando a promessa for resolvida ou rejeitada
+    //     this.carregandoTurmas = false;
+    //   });
+
   },
   methods: {
     returnDadosTurma() {
       return new Promise((resolve, reject) => {
         const token = {
           token: "abcde"
+        };
+
+        axios({ url: 'https://ta-back.onrender.com/dadosTurma', data: token, method: 'POST' })
+          .then((response) => {
+            // Verifica se a resposta foi bem sucedida
+            if (response && response.data) {
+              // Resolva a promessa com os dados da resposta
+              resolve(response.data);
+            } else {
+              // Se a resposta estiver vazia ou não for bem sucedida, rejeite a promessa com uma mensagem de erro
+              reject("Erro: resposta vazia ou não bem sucedida");
+            }
+          })
+          .catch((error) => {
+            // Em caso de erro na solicitação, rejeite a promessa com o erro
+            reject(error);
+          });
+      });
+    },
+
+    returnDadosTurmaNumber(turmaValue) {
+      return new Promise((resolve, reject) => {
+        const token = {
+          token: "abcde",
+          turma: turmaValue
         };
 
         axios({ url: 'https://ta-back.onrender.com/dadosTurma', data: token, method: 'POST' })
@@ -268,22 +295,6 @@ export default {
       console.log(this.toggle);
     },
 
-    calculaHabilidades() {
-      const habilidades = {};
-
-      this.listaturma.listaQuest.forEach(questao => {
-        if (questao.acertou) {
-          const nivel = questao.id.split('_')[1]; // Obter o nível da habilidade (por exemplo, "H06")
-          const numeroNivel = parseInt(nivel.slice(1, 3)); // Extrair o número do nível (por exemplo, 6)
-
-          if (numeroNivel >= 0 && numeroNivel <= 11) {
-            const habilidade = `H${numeroNivel.toString().padStart(2, '0')}`; // Formatar habilidade (por exemplo, "H06" para "H06")
-            habilidades[habilidade] = (habilidades[habilidade] || 0) + 1;
-          }
-        }
-      });
-    },
-
     testaValores(item) {
       // console.log(item);
       this.verificaHabilidades(item.listaQuest, true)
@@ -308,60 +319,14 @@ export default {
 
     },
 
-    getChartData(item) {
-      const listaHabilidades = this.verificaHabilidades(item.listaQuest, true);
-      const labels = Array.from({ length: 12 }, (_, i) => `H${(i).toString().padStart(2, '0')}`);
-
-      const chartData = {
-        labels: labels,
-        datasets: [{
-          label: 'Habilidades',
-          data: listaHabilidades,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(50, 205, 50, 0.2)',
-            'rgba(255, 0, 255, 0.2)',
-            'rgba(0, 191, 255, 0.2)',
-            'rgba(255, 140, 0, 0.2)',
-            'rgba(75, 0, 130, 0.2)'
-
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(50, 205, 50, 1)',
-            'rgba(255, 0, 255, 1)',
-            'rgba(0, 191, 255, 1)',
-            'rgba(255, 140, 0, 1)',
-            'rgba(75, 0, 130, 1)'
-
-          ],
-          borderWidth: 1
-        }]
-      };
-
-      return chartData;
-
-
-
-    },
-
     getChartDatas(lista) {  
-      
-      
+      console.log(lista);
 
       for (let i = 0; i < lista.length; i++) {
         const objeto = lista[i];
+        
         const dadosHabilidades = this.verificaHabilidades(objeto, true);
+        
 
         const labels = Array.from({ length: 12 }, (_, i) => `H${(i).toString().padStart(2, '0')}`);
         const novoChartData = {
@@ -409,6 +374,27 @@ export default {
       
       return lista;
 
+    },
+
+    selecionaTurma(turmaValue){
+
+
+      this.returnDadosTurmaNumber(turmaValue)
+      .then((dados) => {
+        console.log(dados)
+
+        // Quando a promessa for resolvida, atribua os dados à listaTurma
+        this.listaTurma = dados;
+        console.log(this.listaTurma)
+        this.listaTurma = this.getChartDatas(this.listaTurma);
+      })
+      .catch((error) => {
+        // Em caso de erro, trate-o de acordo com a sua necessidade
+        console.error("Erro ao buscar dados da turma:", error);
+      }).finally(() => {
+        // Define carregandoTurmas como false quando a promessa for resolvida ou rejeitada
+        this.carregandoTurmas = false;
+      });
     }
 
   },
@@ -431,6 +417,20 @@ export default {
       }
       else if (this.toggle === "N Questões") {
         return this.listaTurma.slice().sort((a, b) => b.numero_questoes_feitas - a.numero_questoes_feitas);
+      }
+      // Adicione condições semelhantes para outros tipos de filtragem, se necessário
+      return this.listaTurma;
+    },
+
+    filtrarTurma() {
+      if (this.toggleTurma === "t1") {
+        // return this.listaTurma.slice().sort((a, b) => (a.user['nome'] > b.user['nome']) ? 1 : -1);
+        console.log("T1")
+      }
+      else if (this.toggleTurma === "t2") {
+        // const extratoOrder = { '3': 0, '2': 1, '1': 2, '': 3 };
+        // return this.listaTurma.slice().sort((a, b) => extratoOrder[a.extratoFinal] - extratoOrder[b.extratoFinal]);
+        console.log("T1")
       }
       // Adicione condições semelhantes para outros tipos de filtragem, se necessário
       return this.listaTurma;
