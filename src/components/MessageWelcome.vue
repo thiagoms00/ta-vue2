7eebabf31deba3927e45b2f101d9fa7659da2b7a
jadercerqueira <jader58@live.com>
2024-04-19 14:53:17 -0300

    🔄 - Finalizando animação.

--- src/components/MessageWelcome.vue
+++ src/components/MessageWelcome.vue
@@ -7,24 +7,22 @@
 
       <v-container class="d-flex flex-column align-center">
         <div class="d-flex align-center">
-          <v-img :aspect-ratio="1" src="../assets/imgs/prancheto_abertura.png"
-            width="180" class="img-mascote"></v-img>
+          <v-img :aspect-ratio="1" src="../assets/imgs/prancheto_abertura.png" width="180" class="img-mascote"></v-img>
           <h1 class="main-msg"> Olá, seja bem-vindo (a)</h1>
         </div>
 
 
         <v-divider class="my-4 " width="70%"></v-divider>
-        <v-btn @click="highlightComponent">Destacar Componente</v-btn>
-        <Megaphone :class="{ 'wobble-hor-bottom': isHighlighted }" @click="playAudio() " />
+        <Megaphone :class="{ 'wobble-hor-bottom': animaMega }" @click="playAudio()" />
 
         <p class="welcome-text"> Você vai responder algumas questões de Português.</p>
         <p class="welcome-text"> Ouça a orientação do que deve ser feito no início de cada questão.</p>
         <p class="welcome-text"> Se quiser ouvir novamente, clique no megafone.</p>
-        <p class="welcome-text"> Depois, marque a  alternativa que considera correta.</p>
+        <p class="welcome-text"> Depois, marque a alternativa que considera correta.</p>
         <p class="welcome-text"> Para seguir em frente, clique em continuar.</p>
         <p class="welcome-text"> Vamos lá! </p>
-        <v-btn class="flex-grow-1 mt-4 cont-button" height="55" size="large" width="50%" variant="flat" color="#1E3892" elevation-15
-          @click="proximaPagina" :loading="loading"> {{ buttonText }} </v-btn>
+        <v-btn class="flex-grow-1 mt-4 cont-button" :class="{ 'shake-horizontal': animaButton }" height="55" size="large" width="50%" variant="flat" color="#1E3892"
+          elevation-15 @click="proximaPagina" :loading="loading"> {{ buttonText }} </v-btn>
 
       </v-container>
     </v-sheet>
@@ -52,7 +50,8 @@ export default {
       megafoneDisable: false,
       loading: false,
       buttonText: 'Continuar',
-      isHighlighted: false
+      animaMega: false,
+      animaButton: false
     }
   },
 
@@ -74,12 +73,14 @@ export default {
         this.audioAux = new Audio(audiowelcome);
         this.audioAux.play();
         this.megafoneDisable = true;
+        setTimeout(() => {
+          this.animaMega = !this.animaMega;
+        }, 12000);
+        setTimeout(() => {
+          this.animaButton = !this.animaButton;
+        }, 19500);
         setTimeout(this.resetMegafone, 13000);
       }
-    },
-
-    highlightComponent() {
-      this.isHighlighted = !this.isHighlighted; // Alternar o destaque do componente
     },
   }
 
@@ -88,98 +89,173 @@ export default {
 </script>./Megaphone.vue
 
 <style>
-
-
-.main-msg{
+.main-msg {
   margin-right: 20px;
   font-family: 'Manrope';
 }
 
-.welcome-text{
+.welcome-text {
   font-family: 'Manrope';
   font-size: 20px;
   font-weight: 600;
 
 }
 
+.shake-horizontal {
+  -webkit-animation: shake-horizontal 1s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+  animation: shake-horizontal 1s cubic-bezier(0.455, 0.030, 0.515, 0.955) both;
+}
+
+ @-webkit-keyframes shake-horizontal {
+  0%,
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+  }
+  10%,
+  30%,
+  50%,
+  70% {
+    -webkit-transform: translateX(-10px);
+            transform: translateX(-10px);
+  }
+  20%,
+  40%,
+  60% {
+    -webkit-transform: translateX(10px);
+            transform: translateX(10px);
+  }
+  80% {
+    -webkit-transform: translateX(8px);
+            transform: translateX(8px);
+  }
+  90% {
+    -webkit-transform: translateX(-8px);
+            transform: translateX(-8px);
+  }
+}
+@keyframes shake-horizontal {
+  0%,
+  100% {
+    -webkit-transform: translateX(0);
+            transform: translateX(0);
+  }
+  10%,
+  30%,
+  50%,
+  70% {
+    -webkit-transform: translateX(-10px);
+            transform: translateX(-10px);
+  }
+  20%,
+  40%,
+  60% {
+    -webkit-transform: translateX(10px);
+            transform: translateX(10px);
+  }
+  80% {
+    -webkit-transform: translateX(8px);
+            transform: translateX(8px);
+  }
+  90% {
+    -webkit-transform: translateX(-8px);
+            transform: translateX(-8px);
+  }
+}
+
+
 .wobble-hor-bottom {
-	-webkit-animation: wobble-hor-bottom 1s both;
-	animation: wobble-hor-bottom 1s both;
+  -webkit-animation: wobble-hor-bottom 1s both;
+  animation: wobble-hor-bottom 1s both;
 }
- @-webkit-keyframes wobble-hor-bottom {
+
+@-webkit-keyframes wobble-hor-bottom {
+
   0%,
   100% {
     -webkit-transform: translateX(0%);
-            transform: translateX(0%);
+    transform: translateX(0%);
     -webkit-transform-origin: 50% 50%;
-            transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
   }
+
   15% {
     -webkit-transform: translateX(-30px) rotate(-6deg);
-            transform: translateX(-30px) rotate(-6deg);
+    transform: translateX(-30px) rotate(-6deg);
   }
+
   30% {
     -webkit-transform: translateX(15px) rotate(6deg);
-            transform: translateX(15px) rotate(6deg);
+    transform: translateX(15px) rotate(6deg);
   }
+
   45% {
     -webkit-transform: translateX(-15px) rotate(-3.6deg);
-            transform: translateX(-15px) rotate(-3.6deg);
+    transform: translateX(-15px) rotate(-3.6deg);
   }
+
   60% {
     -webkit-transform: translateX(9px) rotate(2.4deg);
-            transform: translateX(9px) rotate(2.4deg);
+    transform: translateX(9px) rotate(2.4deg);
   }
+
   75% {
     -webkit-transform: translateX(-6px) rotate(-1.2deg);
-            transform: translateX(-6px) rotate(-1.2deg);
+    transform: translateX(-6px) rotate(-1.2deg);
   }
 }
+
 @keyframes wobble-hor-bottom {
+
   0%,
   100% {
     -webkit-transform: translateX(0%);
-            transform: translateX(0%);
+    transform: translateX(0%);
     -webkit-transform-origin: 50% 50%;
-            transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
   }
+
   15% {
     -webkit-transform: translateX(-30px) rotate(-6deg);
-            transform: translateX(-30px) rotate(-6deg);
+    transform: translateX(-30px) rotate(-6deg);
   }
+
   30% {
     -webkit-transform: translateX(15px) rotate(6deg);
-            transform: translateX(15px) rotate(6deg);
+    transform: translateX(15px) rotate(6deg);
   }
+
   45% {
     -webkit-transform: translateX(-15px) rotate(-3.6deg);
-            transform: translateX(-15px) rotate(-3.6deg);
+    transform: translateX(-15px) rotate(-3.6deg);
   }
+
   60% {
     -webkit-transform: translateX(9px) rotate(2.4deg);
-            transform: translateX(9px) rotate(2.4deg);
+    transform: translateX(9px) rotate(2.4deg);
   }
+
   75% {
     -webkit-transform: translateX(-6px) rotate(-1.2deg);
-            transform: translateX(-6px) rotate(-1.2deg);
+    transform: translateX(-6px) rotate(-1.2deg);
   }
 }
 
 
 
 @media(max-width: 1550px) {
-  
 
-  .welcome-text{
+
+  .welcome-text {
     font-size: 13px;
   }
 
-  .img-mascote{
+  .img-mascote {
     width: 100px !important;
-    
+
   }
-  
-  .cont-button{
+
+  .cont-button {
     height: 7vh !important;
     font-size: 14px !important;
   }
@@ -187,7 +263,4 @@ export default {
 
 
 }
-
-
-
 </style>
