6aea9d4a7d52dcdd388965fadf9b88b4d010d51a
thiagoms00 <menezes00.Thiago@gmail.com>
2024-08-30 09:03:40 -0300

    responsividade nas configs

--- src/components/TurmaConfig.vue
+++ src/components/TurmaConfig.vue
@@ -7,7 +7,7 @@
  -->              <h1 class="ml-2 config-title">Configurações da Turma</h1>
             </div>
             <div class="change-est mt-5 d-flex elevation-1">
-              <div class="d-flex flex-column">
+              <div class="option-texts d-flex flex-column">
                 <h1 class="change-text font-weight-black">Alterar estrato inicial</h1>
                 <p class="change-est-p font-weight-medium">Escolha o primeiro estrato à ser avaliado no teste adaptativo.</p>
               </div>
@@ -15,8 +15,14 @@
                 <v-btn class="change-button elevation-5" icon="mdi-numeric-1" :color="corBtn[0]" @click="mudaEstrato(1)"></v-btn>
                 <v-btn class="change-button elevation-5" icon="mdi-numeric-2" :color="corBtn[1]" @click="mudaEstrato(2)"></v-btn>
               </div>
-           
-             
+            </div>
+
+            <div class="change-est mt-5 d-flex elevation-1">
+              <div class="option-texts d-flex flex-column">
+                <h1 class="change-text font-weight-black">Permitir testes</h1>
+                <p class="change-est-p font-weight-medium">Possibilita que alunos desta turma possam iniciar o teste adaptativo.</p>
+              </div>
+                <v-switch class="switch-start" color="indigo" label="" inset></v-switch>
             </div>
 
            
@@ -63,7 +69,6 @@
       // Observa mudanças em `listaDeAlunos`
       listaDeAlunos(newVal) {
         this.listaTurma = newVal;
-        this.gerarDadosGrafico();
       },
     },
   
@@ -99,8 +104,12 @@
   }
 
   .config-title{
-    font-size: 2.4rem;
+    font-size: 2.1rem;
     color: black;
+  }
+
+  .option-texts{
+    width: 90% !important;
   }
 
   .change-est{
@@ -113,22 +122,105 @@
 
   .change-text{
     color: black;
-    font-size: 1.7rem;
+    font-size: 1.4rem;
 
   }
 
   .change-est-p{
     color: rgb(0, 0, 0);
-    font-size: 1.1rem;
+    font-size: 1.0rem;
   }
 
   .change-button{
    background-color: rgb(199, 199, 255);
   }
 
-  .buttons{
-    margin-left: 35vw !important;
+  .switch-start .v-input__control{
+    margin-top: 1vh;
   }
+
+
+@media (max-width: 1600px) {
+  
+
+  .config-title{
+    font-size: 1.5rem;
+  }
+
+  .option-texts{
+    width: 90% !important;
+  }
+
+  .change-est{
+    border: 0.05rem solid rgba(102, 102, 103, 0.691);  
+    padding: 1vh 1vw;
+    border-radius: 5px;
+    background-color: rgb(255, 255, 255);
+    height: 10vh !important;
+  }
+
+  .change-text{
+    color: black;
+    font-size: 1.1rem;
+
+  }
+
+  .change-est-p{
+    color: rgb(0, 0, 0);
+    font-size: 0.85rem;
+  }
+
+  .change-button{
+    width: 2.5vw !important;
+    height: 2.5vw !important;
+  }
+
+  
+  
+  .switch-start .v-input__control{
+    margin-top: 0vh;
+  }
+
+}
+
+@media (max-width: 1300px) {
+  
+
+  .config-title{
+    font-size: 1.3rem;
+  }
+
+  .option-texts{
+    width: 90% !important;
+  }
+
+  
+  .change-text{
+    color: black;
+    font-size: 1.0rem;
+
+  }
+
+  .change-est-p{
+    color: rgb(0, 0, 0);
+    font-size: 0.75rem;
+  }
+  .buttons{
+    padding-bottom: 0.5vh !important;
+  }
+
+  .change-button{
+    width: 2.7vw !important;
+    height: 2.7vw !important;
+  }
+
+  
+  
+  .switch-start .v-input__control{
+    margin-top: 0vh;
+  }
+
+}
 
 </style>
   
