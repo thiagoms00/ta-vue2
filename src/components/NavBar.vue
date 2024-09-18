63462c30971cd72c2acc0e841680a8cf6ec68493
Jader Cerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2024-09-18 10:55:46 -0300

    ♻️ - Ajuste na navbar.
    Agora a navbar acompanha o tamanho da parte do conteudo.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,35 +1,40 @@
-<template><v-app-bar flat color="#1E3892">
-        <v-container class="mx-auto d-flex align-center justify-center">
-            <v-avatar class="me-4 " color="blue-lighten-4" size="32"></v-avatar>
+<template>
+  <v-app-bar flat color="#1E3892">
+    <v-container fluid class="mx-auto">
+      <v-row class="d-flex justify-center">
+        <v-col cols="11" class="d-flex align-center justify-center">
+          <v-avatar class="me-4" color="blue-lighten-4" size="32"></v-avatar>
 
-            <v-btn v-for="link in links" :key="link" :text="link" variant="text"></v-btn>
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            :text="link"
+            variant="text"
+          ></v-btn>
 
-            <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-            <v-btn dark @click="logout" prepend-icon="mdi-logout-variant">
-                SAIR
-            </v-btn>
-
-        </v-container>
-    </v-app-bar>
+          <v-btn dark @click="logout" prepend-icon="mdi-logout-variant">
+            SAIR
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
 </template>
 
 <script setup>
-const links = [
-    'Dashboard',
-    'Turmas',
-    'Atualizações',
-]
+const links = ["Dashboard", "Turmas", "Atualizações"];
 </script>
 
 <script>
 export default {
-    name: 'NavBar',
-    methods: {
-        logout() {
-            localStorage.clear();
-            this.$router.push('/proflogin');
-        },
-    }
-}
+  name: "NavBar",
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push("/proflogin");
+    },
+  },
+};
 </script>
