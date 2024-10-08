c1b773e7cddbda2bd633bf06e748b82618c5693b
Jader Cerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2024-10-07 23:02:12 -0300

    ♻️ - Otimizando método de login. pt2

--- src/views/LoginLogin.vue
+++ src/views/LoginLogin.vue
@@ -1,32 +1,29 @@
 <template>
   <v-container class="main-div" fluid>
     <v-row class="full-height d-flex justify-center align-center">
-      <v-col cols="3" sm="2" md="3" class="text-center">
-      </v-col>
+      <v-col cols="3" sm="2" md="3" class="text-center"> </v-col>
 
       <v-col cols="6" sm="8" md="6" class="text-center">
         <v-sheet class="rounded-lg elevation-2 pb-14">
-          
           <!-- Parte da Logo do planoA -->
           <v-row class="d-flex justify-center">
             <v-col cols="8" sm="6" md="4" xs="3">
-                <img
-                  src="../assets/imgs/Logo-PlanoA-removebg.png"
-                  alt="Logo Plano A"
-                  class="logo-image-planoa"
-                />
+              <img
+                src="../assets/imgs/Logo-PlanoA-removebg.png"
+                alt="Logo Plano A"
+                class="logo-image-planoa"
+              />
             </v-col>
           </v-row>
 
           <!-- Parte do formulário de login. -->
           <v-row class="d-flex justify-center">
-            <v-col cols="8" >
-              <div class="text-h2 text-md-h2 text-sm-h4 text-xs-h5 d-flex px-4">Login</div>
+            <v-col cols="8">
+              <div class="text-h2 text-md-h2 text-sm-h4 text-xs-h5 d-flex px-4">
+                Login
+              </div>
               <div>
-                <v-form
-                  v-model="valid"
-                  @submit.prevent="submitForm"
-                >
+                <v-form v-model="valid" @submit.prevent="submitForm">
                   <v-container class="">
                     <v-text-field
                       class="form-field aluno-field"
@@ -75,27 +72,32 @@
         </v-sheet>
       </v-col>
 
-      <v-col cols="3" sm="2" md="3" class="full-height d-flex justify-end align-end">
-        <v-img class="logo-imp"
-        src="../assets/imgs/Logo-Impacto-removebg.png"
-        contain
-        :max-width="300"
-        aspect-ratio="16/9"
-    ></v-img> 
+      <v-col
+        cols="3"
+        sm="2"
+        md="3"
+        class="full-height d-flex justify-end align-end"
+      >
+        <v-img
+          class="logo-imp"
+          src="../assets/imgs/Logo-Impacto-removebg.png"
+          contain
+          :max-width="300"
+          aspect-ratio="16/9"
+        ></v-img>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import LoginForm from "@/components/LoginForm.vue";
 import LogoImpacto from "@/components/LogoImpacto.vue";
 import axios from "axios";
+import { mapActions } from 'vuex'
 
 export default {
   name: "LoginLogin",
   components: {
-    LoginForm,
     LogoImpacto,
   },
   data: () => ({
@@ -125,42 +127,49 @@ export default {
   }),
 
   mounted() {
-    this.verificaLogin();
+    // this.verificaLogin();
   },
 
   methods: {
+
+    ...mapActions([
+      'login'
+    ]),
+
     verificaLogin() {
-  const dataToken = localStorage.getItem("token");
+      const dataToken = localStorage.getItem("token");
 
-  if (!dataToken) {
-    console.log('O localStorage está vazio ou o item "token" não está definido.');
-    this.$router.push("/login");
-    return;
-  }
-
-  const data = { token: dataToken };
-
-  axios.post("https://ta-back.onrender.com/alunos/verificaToken", data)
-    .then((response) => {
-      if (response.status === 200) {
-        // Token é válido
-        this.$router.push("/welcome");
+      if (!dataToken) {
+        console.log(
+          'O localStorage está vazio ou o item "token" não está definido.'
+        );
+        this.$router.push("/login");
+        return;
       }
-    })
-    .catch((error) => {
-      console.error('Erro na verificação do token:', error.response?.status || error.message);
-      localStorage.removeItem("token");
-      this.$router.push("/login");
-    });
-}
-,
 
+      const data = { token: dataToken };
+
+      axios
+        .post("https://ta-back.onrender.com/alunos/verificaToken", data)
+        .then((response) => {
+          if (response.status === 200) {
+            // Token é válido
+            this.$router.push("/welcome");
+          }
+        })
+        .catch((error) => {
+          console.error(
+            "Erro na verificação do token:",
+            error.response?.status || error.message
+          );
+          localStorage.removeItem("token");
+          this.$router.push("/login");
+        });
+    },
     submitForm() {
-      this.buttonText = "Carregando...";
-      this.loading = true;
-      this.error = "";
+      this.setLoadingState(true, "Carregando...");
+      this.clearError();
 
-      // Processamento do formulário
       const userData = {
         id: this.email,
         senha: this.senha,
@@ -168,24 +177,32 @@ export default {
 
       this.login(userData)
         .then(() => {
-          // Ação a ser executada após o login bem-sucedido
           console.log("Login bem-sucedido");
           this.$router.push("/welcome");
         })
         .catch((error) => {
-          // Ação a ser executada se ocorrer um erro durante o login
-          console.error("Erro no login:", error);
-          this.error =
-            "Usuário ou senha incorretos. Por favor, tente novamente.";
-          this.loading = false;
-          this.buttonText = "Entrar";
-
-          setTimeout(() => {
-            // Define loading para false para esconder o botão de carregamento
-            this.error = "";
-            // Aqui você pode adicionar lógica para tratar a resposta do backend
-          }, 5000);
+          this.handleLoginError(error);
         });
+    },
+
+    setLoadingState(isLoading, buttonText) {
+      this.loading = isLoading;
+      this.buttonText = buttonText;
+    },
+
+    clearError() {
+      this.error = "";
+    },
+
+    handleLoginError(error) {
+      console.error("Erro no login:", error);
+      this.error = "Usuário ou senha incorretos. Por favor, tente novamente.";
+      this.setLoadingState(false, "Entrar");
+
+      setTimeout(() => {
+        this.clearError();
+        // Adicionar lógica adicional para tratar a resposta do backend aqui
+      }, 5000);
     },
   },
 };
