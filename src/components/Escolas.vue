3a6ac6d225bab3cb450f15bf0449234a8371b29a
Jader Cerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2024-11-05 19:46:48 -0300

    ➕ - Componentizando submenu de adm

---
+++ src/components/Escolas.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="d-flex flex-column w-100">
+    <p
+      class="text-overline mt-16 d-flex justify-center"
+      style="
+        color: #cfd8dc;
+        font-size: 3rem !important;
+        text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+      "
+    >
+      Selecione uma escola
+    </p>
+
+    <div class="mt-16">
+      <v-row>
+        <v-col
+          v-for="(escola, index) in escolas"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <v-sheet
+            elevation="3"
+            rounded="lg"
+            class="pa-4 d-flex flex-column align-center borda-diagonal degrade"
+          >
+            <v-avatar size="64" class="mb-2">
+              <v-icon large>mdi-school</v-icon>
+            </v-avatar>
+            <div class="text-center">{{ escola.nome }}</div>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Escolas",
+  components: {
+
+  },
+
+  data: () => ({
+    tab: "option-1",
+    habilidadesTurmaAtual: {},
+    escolas: [
+      { nome: "Escola A" },
+      { nome: "Escola B" },
+      { nome: "Escola C" },
+      { nome: "Escola D" },
+    ],
+  }),
+
+  created() {},
+
+  methods: {},
+  // FIM DO METHODSSSSSSS
+
+  computed: {},
+};
+</script>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -43,7 +43,6 @@
                     style="width: 100%"
                   >
                     <v-tab
-                      @click="changeItens(false)"
                       value="option-1"
                       class="pl-4 d-flex justify-start"
                       prepend-icon="mdi-town-hall"
@@ -52,18 +51,16 @@
                     </v-tab>
 
                     <v-tab
-                      @click="changeItens(true)"
                       prepend-icon="mdi-database-outline"
-                      value="option-7"
+                      value="option-2"
                       class="pl-4 d-flex justify-start"
                     >
                       <span>Banco de Itens</span>
                     </v-tab>
 
                     <v-tab
-                      @click="changeItens(false)"
                       prepend-icon="mdi-account-clock-outline"
-                      value="option-4"
+                      value="option-3"
                       class="pl-4 d-flex justify-start"
                     >
                       <span>Logs de Login</span>
@@ -91,88 +88,36 @@
                     </v-tab> -->
                   </v-tabs>
                 </v-sheet>
-                <!-- Campo de busca de aluno -->
               </v-col>
 
               <v-col>
                 <!-- MENSAGEM DE SELEÇÂO DE TURMA -->
                 <div>
                   <v-sheet
-                    v-if="!controlOptions && !controlItens"
                     class="rounded-lg d-flex pa-4 elevation-4"
                     color="grey-lighten-5"
-                    height="500"
+                    min-height="500"
                   >
-                    <div class="d-flex flex-column w-100" >
-                      <p
-                        class="text-overline mt-16 d-flex justify-center"
-                        style="
-                          color: #cfd8dc;
-                          font-size: 3rem !important;
-                          text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
-                        "
-                      >
-                        Selecione uma escola
-                      </p>
+                    <v-window v-model="tab" class="w-100">
+                      <!-- Janela de Dados 1 -->
+                      <v-window-item value="option-1">
+                        <Escolas/>
+                      </v-window-item>
 
-                      <div class="mt-16">
-                        <v-row>
-                          <v-col
-                            v-for="(escola, index) in escolas"
-                            :key="index"
-                            cols="12"
-                            sm="6"
-                            md="4"
-                            lg="3"
-                          >
-                            <v-sheet
-                              elevation="3"
-                              rounded="lg"
-                              class="pa-4 d-flex flex-column align-center 
-                              borda-diagonal degrade"
-                            >
-                              <v-avatar size="64" class="mb-2">
-                                <v-icon large>mdi-school</v-icon>
-                              </v-avatar>
-                              <div class="text-center">{{ escola.nome }}</div>
-                            </v-sheet>
-                          </v-col>
-                        </v-row>
-                      </div>
-                    </div>
+                      <!-- Janela de Dados 2 -->
+                      <v-window-item value="option-2">
+                        TELA 2
+                      </v-window-item>
+
+                      <!-- Janela de Dados 3 -->
+
+                      <v-window-item value="option-3">
+                        TELA 3
+                      </v-window-item>
+                    </v-window>
+
                   </v-sheet>
                 </div>
-
-                <v-window v-model="tab">
-                  <!-- Janela de Dados 1 -->
-                  <v-window-item value="option-1" v-if="controlOptions">
-                    <TurmaNiveisInfo
-                      :listaDeAlunos="listaFiltrada"
-                      :nomeTurma="nomeTurmaSelecionada"
-                      ref="turmaVisualInfo"
-                    />
-                  </v-window-item>
-
-                  <!-- Janela de Dados 2 -->
-                  <v-window-item value="option-2" v-if="controlOptions">
-                    <TurmaMapInfo
-                      :listaDeAlunos="listaFiltrada"
-                      :anoTurma="anoTurma"
-                      :nomeTurma="nomeTurmaSelecionada"
-                      ref="turmaMapInfo"
-                    />
-                  </v-window-item>
-
-                  <!-- Janela de Dados 3 -->
-
-                  <v-window-item value="option-3" v-if="controlOptions">
-                    <TurmaDataInfo
-                      :listaDeAlunos="listaFiltrada"
-                      ref="turmaDataInfo"
-                      @eventDeleteTest="excluirTeste"
-                    />
-                  </v-window-item>
-                </v-window>
               </v-col>
             </v-row>
           </v-col>
@@ -184,45 +129,18 @@
 
 <script>
 import axios from "axios";
-// import Chart from '@/components/Chart.vue'
-import ChartBar from "@/components/ChartBar.vue";
-import DialogExcluirTeste from "@/components/DialogExcluirTeste.vue";
-import TurmaDataInfo from "@/components/TurmaDataInfo.vue";
 import NavBar from "@/components/NavBar.vue";
 import * as XLSX from "xlsx";
-import TurmaMapInfo from "@/components/TurmaMapInfo.vue";
-import TurmaNiveisInfo from "@/components/TurmaNiveisInfo.vue";
-import TurmaGraphInfo from "@/components/TurmaGraphInfo.vue";
-import TurmaConfig from "@/components/TurmaConfig.vue";
-import TurmaCompara from "@/components/TurmaCompara.vue";
-import TurmaItensInfo from "@/components/TurmaItensInfo.vue";
-import { verificaLogin } from "@/utils/helpers";
+import Escolas from "@/components/Escolas.vue";
 
 export default {
   name: "Turma",
   components: {
-    // Chart,
-    ChartBar,
-    NavBar,
-    DialogExcluirTeste,
-    TurmaDataInfo,
-    TurmaMapInfo,
-    TurmaNiveisInfo,
-    TurmaGraphInfo,
-    TurmaConfig,
-    TurmaCompara,
-    TurmaItensInfo,
+    Escolas
   },
 
   data: () => ({
     tab: "option-1",
-    habilidadesTurmaAtual: {},
-    escolas: [
-      { nome: "Escola A" },
-      { nome: "Escola B" },
-      { nome: "Escola C" },
-      { nome: "Escola D" },
-    ],
   }),
 
   created() {},
@@ -242,8 +160,9 @@ export default {
 .degrade {
   background: linear-gradient(
     to bottom right,
-    #e3f2fd,   /* Azul bem claro no canto superior esquerdo */
-    #cfd8dc    /* Cinza claro no canto inferior direito */
+    #e3f2fd,
+    /* Azul bem claro no canto superior esquerdo */ #cfd8dc
+      /* Cinza claro no canto inferior direito */
   );
 }
 </style>
