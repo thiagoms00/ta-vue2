e9ea29a10e28e9d2e660401098ab36e0a1fa291f
Jader Cerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2024-11-19 10:23:12 -0300

    ♻️ - Lógica de iteração de escolas e turmas mudada.

--- src/components/ListadeEscolas.vue
+++ src/components/ListadeEscolas.vue
@@ -1,34 +1,12 @@
 <template>
   <!-- Sheet que engloba todo o componente -->
-  <v-sheet
-    elevation="2"
-    class="d-flex flex-column rounded-b-lg"
-    color="white"
-  >
+  <v-sheet elevation="2" class="d-flex flex-column rounded-b-lg" color="white">
     <div class="d-flex flex-column w-100">
       <v-row>
-        <!-- Avatar de adicionar nova escola -->
-        <v-col cols="12" sm="6" md="4" lg="3" class="pt-8 pb-8 pl-8 d-flex align-center justify-center">
-          <v-sheet
-            height="200"
-            
-            elevation="2"
-            rounded="lg"
-            class="pa-4 d-flex flex-column align-center borda-diagonal clickable-item position-relative justify-center degrade"
-          >
-            <div class="d-flex flex-column align-center">
-              <v-avatar size="70" class="mb-3" color="white">
-                <v-icon size="x-large" color="primary">mdi-plus</v-icon>
-              </v-avatar>
-              <div class="text-center text-body-2">Adicionar Nova Escola</div>
-            </div>
-          </v-sheet>
-        </v-col>
-
         <!-- Skeleton Loader enquanto os dados estão carregando -->
         <v-col
           v-if="isLoading"
-          v-for="n in 3"
+          v-for="n in 4"
           :key="n"
           cols="12"
           sm="6"
@@ -47,7 +25,7 @@
         <!-- Iteração das escolas existentes quando carregadas -->
         <v-col
           v-if="!isLoading"
-          v-for="(escola, index) in escolas"
+          v-for="(escola, index) in escolasFinal"
           :key="index"
           cols="12"
           sm="6"
@@ -58,10 +36,9 @@
           <v-sheet
             elevation="2"
             height="200"
-            
             rounded="lg"
             class="pa-4 d-flex flex-column align-center borda-diagonal degrade clickable-item position-relative justify-center"
-            @click="fetchTurmas(index, escola.id_escola)"
+            @click="handleClick(escola, index)"
             style="cursor: pointer"
           >
             <!-- Loader circular quando o card específico está em carregamento -->
@@ -70,16 +47,26 @@
               class="d-flex align-center justify-center degrade borda-diagonal"
               style="position: absolute; inset: 0; z-index: 1"
             >
-              <v-progress-circular indeterminate color="white" size="30"></v-progress-circular>
+              <v-progress-circular
+                indeterminate
+                color="white"
+                size="30"
+              ></v-progress-circular>
             </div>
 
             <!-- Conteúdo do card (avatar e textos), que some ao clicar -->
             <template v-else>
               <v-avatar size="70" color="white" class="mb-3">
-                <v-icon size="x-large" color="primary">mdi-school</v-icon>
+                <v-icon size="x-large" color="primary">{{
+                  escola.nome_escola === "Adicionar Nova Escola"
+                    ? "mdi-plus"
+                    : "mdi-school"
+                }}</v-icon>
               </v-avatar>
               <div class="d-flex flex-column escola-info">
-                <div class="text-center text-body-2 nome-escola">{{ escola.nome_escola }}</div>
+                <div class="text-center text-body-2 nome-escola">
+                  {{ escola.nome_escola }}
+                </div>
                 <div class="text-center text-subtitle-2 mt-2 nome-diretor">
                   {{ escola.diretor_nome }}
                 </div>
@@ -91,8 +78,6 @@
     </div>
   </v-sheet>
 </template>
-
-
 
 <script>
 import axios from "axios";
@@ -113,6 +98,15 @@ export default {
     },
   },
 
+  computed: {
+    escolasFinal() {
+      return [
+        { novaEscola: true, nome_escola: "Adicionar Nova Escola" },
+        ...this.escolas,
+      ];
+    },
+  },
+
   watch: {
     escolas: {
       handler(newVal) {
@@ -129,6 +123,14 @@ export default {
   },
 
   methods: {
+    handleClick(escola, index) {
+      if (escola.nome_escola === "Adicionar Nova Escola") {
+        console.log("Adicionar nova turma");
+      } else {
+        this.fetchTurmas(index, escola.id_escola);
+      }
+    },
+
     fetchTurmas(index, id) {
       // Ativa o carregamento para o card específico pelo índice
       this.isCardLoading[index] = true;
@@ -163,17 +165,16 @@ export default {
 </script>
 
 <style scoped>
-.escola-info{
+.escola-info {
   border-radius: 0.3rem;
   overflow: hidden;
   padding: 2px;
 }
 
-.nome-escola{
+.nome-escola {
   font-size: 1.02rem !important;
-  font-family: 'Urbanist-Regular';
+  font-family: "Urbanist-Regular";
   font-weight: 600;
-  margin-top:0.3vh;
+  margin-top: 0.3vh;
 }
-
 </style>
--- src/components/ListadeTurmas.vue
+++ src/components/ListadeTurmas.vue
@@ -1,36 +1,21 @@
 <template>
   <!-- Sheet que engloba todo o componente -->
-  <v-sheet
-    elevation="2"
-    class="d-flex flex-column rounded-b-lg"
-    color="white"
-  >
+  <v-sheet elevation="2" class="d-flex flex-column rounded-b-lg" color="white">
     <div class="d-flex flex-column w-100">
       <div>
         <v-row class="pa-8">
-          <!-- Botão para adicionar nova turma -->
-          <v-col cols="12" sm="4">
-            <v-sheet
-              elevation="3"
-              rounded="lg"
-              class="pa-4 d-flex align-center borda-diagonal degrade clickable-item"
-              style="cursor: pointer"
-              height="100"
-            >
-              <v-avatar size="50" color="white">
-                <v-icon size="x-large" color="primary">mdi-plus</v-icon>
-              </v-avatar>
-              <div class="text-center text-body-2 mx-4">Adicionar Nova Turma</div>
-            </v-sheet>
-          </v-col>
-
           <!-- Iteração das turmas existentes -->
-          <v-col v-for="(turma, index) in turmas" :key="index" cols="12" sm="4">
+          <v-col
+            v-for="(turma, index) in turmasFinal"
+            :key="index"
+            cols="12"
+            sm="4"
+          >
             <v-sheet
               elevation="3"
               rounded="lg"
               class="pa-4 d-flex align-center borda-diagonal degrade clickable-item"
-              @click="selecionaTurma(turma._id, index)"
+              @click="handleCardClick(turma, index)"
               style="cursor: pointer"
               height="100"
             >
@@ -48,32 +33,58 @@
 
               <!-- Conteúdo do Card -->
               <template v-else>
-                <v-avatar size="50" color="white" class="mr-4 hidden-sm-and-down">
-                  <v-icon size="large" color="primary">mdi-account-group</v-icon>
-                </v-avatar>
-                <div class="mx-2">
-                  <div>
-                    Nome: <span class="font-weight-bold">{{ turma.nome }}</span>
+                <!-- Card "Adicionar Nova Turma" -->
+                <template v-if="turma.nome === 'Adicionar Nova Turma'">
+                  <v-avatar
+                    size="50"
+                    color="white"
+                    class="mr-4 hidden-sm-and-down"
+                  >
+                    <v-icon size="large" color="primary">mdi-plus</v-icon>
+                  </v-avatar>
+                  <div class="mx-2">
+                    <div class="font-weight-bold text-center">
+                      Adicionar Nova Turma
+                    </div>
                   </div>
-                  <div>
-                    Número de alunos:
-                    <span class="font-weight-bold">{{
-                      turma.numero_alunos
-                    }}</span>
-                  </div>
-                </div>
-                <div class="ml-5 hidden-lg-and-down">
-                  <div>Professores:</div>
-                  <div>
-                    <span
-                      v-for="(professor, profIndex) in turma.listaDeProfs"
-                      :key="profIndex"
-                      class="font-weight-bold"
+                </template>
+
+                <!-- Card padrão para as outras turmas -->
+                <template v-else>
+                  <v-avatar
+                    size="50"
+                    color="white"
+                    class="mr-4 hidden-sm-and-down"
+                  >
+                    <v-icon size="large" color="primary"
+                      >mdi-account-group</v-icon
                     >
-                      {{ professor }}
-                    </span>
+                  </v-avatar>
+                  <div class="mx-2">
+                    <div>
+                      Nome:
+                      <span class="font-weight-bold">{{ turma.nome }}</span>
+                    </div>
+                    <div>
+                      Número de alunos:
+                      <span class="font-weight-bold">{{
+                        turma.numero_alunos
+                      }}</span>
+                    </div>
                   </div>
-                </div>
+                  <div class="ml-5 hidden-lg-and-down">
+                    <div>Professores:</div>
+                    <div>
+                      <span
+                        v-for="(professor, profIndex) in turma.listaDeProfs"
+                        :key="profIndex"
+                        class="font-weight-bold"
+                      >
+                        {{ professor }}
+                      </span>
+                    </div>
+                  </div>
+                </template>
               </template>
             </v-sheet>
           </v-col>
@@ -82,7 +93,6 @@
     </div>
   </v-sheet>
 </template>
-
 
 <script>
 import axios from "axios";
@@ -102,6 +112,15 @@ export default {
     },
   },
 
+  computed: {
+    turmasFinal() {
+      return [
+        { novaTurma: true, nome: "Adicionar Nova Turma" },
+        ...this.turmas,
+      ];
+    },
+  },
+
   watch: {
     turmas: {
       handler(newVal) {
@@ -115,6 +134,14 @@ export default {
   },
 
   methods: {
+    handleCardClick(turma, index) {
+      if (turma.nome === "Adicionar Nova Turma") {
+        console.log("Adicionar nova turma");
+      } else {
+        this.selecionaTurma(turma._id, index);
+      }
+    },
+
     selecionaTurma(turmaId, index) {
       // Define o loader apenas para o card clicado
       this.isCardLoading[index] = true;
@@ -132,7 +159,7 @@ export default {
       })
         .then((response) => {
           this.dadosTurma = response.data;
-          console.log(this.dadosTurma)
+          console.log(this.dadosTurma);
           this.$emit("changeLevel", "turmaselecionado", this.dadosTurma);
         })
         .catch((error) => {
